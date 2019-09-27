370a9e2641dc737b7d5e9bc07095e9e69220ee9c
lucalarsen <lucalarsen@hotmail.com>
2019-09-27 13:21:24 +0200

    Add files via upload

---
+++ RestCoinService/RestCoinService/Controllers/CoinController.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using RestCoinService.Model;
+
+namespace RestCoinService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinController : ControllerBase
+    {
+        #region original
+        //// GET: api/Coin
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Coin/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //// POST: api/Coin
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/Coin/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+
+        #endregion
+
+        private static List<Coin> cList;
+        public static int nextid = 0;
+
+        static CoinController()
+        {
+           // id = 0;
+            cList = new List<Coin>();
+            Coin coin1 = new Coin("Gold DK 1640", 2500, "Mike");
+            Coin coin2 = new Coin("Gold NL 1764", 5000, "Anbo");
+            Coin coin3 = new Coin("Gold FR1644", 35000, "Hammer");
+
+            cList.Add(coin1);
+            cList.Add(coin2);
+            cList.Add(coin3);
+        }
+
+        // GET: api/Coin
+        [HttpGet]
+        public List<Coin> Get()
+        {
+            return cList;
+        }
+
+        // GET: api/Coin/
+        [HttpGet("{id}", Name = "Get")]
+        public Coin Get(int id)
+        {
+            var item = cList.SingleOrDefault(c => c.ID == id);
+            return item;
+        }
+
+        //POST: api/Coin
+        [HttpPost]
+        public Coin InsertCoin([FromBody] Coin coin)
+        {
+            Coin c = coin;
+            //c.ID = nextid++;
+            cList.Add(coin);
+            return coin;
+        }
+
+    }
+}
---
+++ RestCoinService/RestCoinService/Model/Coin.cs
@@ -0,0 +1,58 @@
+﻿using RestCoinService.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RestCoinService.Model
+{
+    public class Coin
+    {
+        private int _id;
+        //public static int nextid = 1;
+        public int ID
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        private string _genstand;
+
+        public string Genstand
+        {
+            get { return _genstand; }
+            set { _genstand = value; }
+        }
+
+        private int _bud;
+
+        public int Bud
+        {
+            get { return _bud; }
+            set { _bud = value; }
+        }
+
+        private string _navn;
+
+        public string Navn
+        {
+            get { return _navn; }
+            set { _navn = value; }
+        }
+
+        public Coin(string genstand, int bud, string navn)
+        {
+            Genstand = genstand;
+            Bud = bud;
+            Navn = navn;
+            ID = CoinController.nextid++;
+        }
+
+        public Coin()
+        {
+            ID = CoinController.nextid++;
+        }
+
+
+    }
+}
